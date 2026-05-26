f96dc7cd00ca7df15e2b5c3e8f6041a32d1b70a7
Bhavesh Budharaju <122691714+Bhavesh2k4@users.noreply.github.com>
2026-05-26 22:47:30 +0530

    Add: find-the-town-judge - submission-1

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> out(n+1,0);
+        vector<int> in(n+1,0);
+        for(int i=0;i<trust.size();i++){
+            //similar to creating adjacency list without creating one
+            out[trust[i][0]]++;
+            in[trust[i][1]]++;
+        }
+        for(int i=1;i<n+1;i++){
+            if(in[i]==n-1 && out[i]==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
