36162e39daa4c239b19e3592a025ca4985dd8878
Bhavesh Budharaju <122691714+Bhavesh2k4@users.noreply.github.com>
2026-05-27 22:50:26 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int,list<int>> adj;
+        vector<int> indegree(numCourses);
+        for(int i=0;i<prerequisites.size();i++){
+            int a=prerequisites[i][0];
+            int b=prerequisites[i][1];
+            indegree[a]++;
+            adj[b].push_back(a);
+        }
+        queue<int> q;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        int c=0;
+        while(!q.empty()){
+            int top=q.front();
+            q.pop();
+            c++;
+            for(int x:adj[top]){
+                indegree[x]--;
+                if(indegree[x]==0){
+                    q.push(x);
+                }
+            }
+        }
+        if(c==numCourses){
+            return true;
+        }
+        return false;
+    }
+};
